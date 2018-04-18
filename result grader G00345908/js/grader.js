b15ebb443add38e7e1cc6dc1330d289d02de4d7d
keighlum <35769357+keighlum@users.noreply.github.com>
2018-04-18 14:22:14 +0100

    Add files via upload

---
+++ result grader G00345908/js/grader.js
@@ -0,0 +1,19 @@
+function gradeResult() {
+	var myResult = parseInt(document.getElementById("value1").value);
+	var grade;
+	if (!myResult) {
+		alert("please enter result");
+	}
+	else {
+		if (myResult > 90) {
+			grade = 'A';
+		} else if (myResult > 80) {
+			grade = 'B' ;
+		} else {
+			grade = 'F'
+		}
+		var message = 'your result of ' + myResult + ' gets you a grade ' + grade;
+		console.log(message);
+		document.getElementById("resultsentence").innerHTML = message;
+	}
+}
